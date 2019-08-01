5e67baa246d42ab2e10476307a18ad40da011553
Viktor <test@email.haha>
2019-08-01 20:02:37 +0500

    Поменял главную

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-git
+                    Laravel-gitt
                 </div>
 
                 <div class="links">
