eb133c66a8628460c098c95eefd0455271ff9bac
Viktor <test@email.haha>
2019-08-01 20:15:47 +0500

    Поправил главную

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-gitt
+                    Laravel-git
                 </div>
 
                 <div class="links">
